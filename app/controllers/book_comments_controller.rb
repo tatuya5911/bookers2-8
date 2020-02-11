5d8583abecb2d1929e05791b3da854e07090f267
tatuya5911 <kkb76904@gmail.com>
2020-02-11 06:21:09 +0000

    finish

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,14 +5,13 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    redirect_to book_path(current_user.id)
   end
 
   def destroy
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.find_by(book_id: book.id)
-    comment.destroy!
-    redirect_back(fallback_location: root_path)
+    @book_comment = current_user.book_comments.find_by(book_id: book.id)
+    @book_comment.destroy!
   end
 
   private
